49613225b6514d95fc8c4141de9e74da3e9a2978
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2021-04-17 18:25:03 +0300

    part 3

--- app/src/main/java/fi/oamk/chat/MainActivity.kt
+++ app/src/main/java/fi/oamk/chat/MainActivity.kt
@@ -3,12 +3,19 @@ package fi.oamk.chat
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -17,10 +24,13 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = MainActivity::class.java.name
     private lateinit var  messages: ArrayList<String>
     private lateinit var  database: DatabaseReference
     private lateinit var  edMessage: EditText
     private lateinit var  rcMessageList: RecyclerView
+    private lateinit var  auth : FirebaseAuth
+    private var currentUser: FirebaseUser? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         rcMessageList = findViewById(R.id.messageList)
 
         database = Firebase.database.reference
+        auth = Firebase.auth
         messages = arrayListOf()
 
         edMessage.setOnKeyListener{ v, keyCode, event ->
@@ -77,5 +88,51 @@ class MainActivity : AppCompatActivity() {
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
+    }
+
+    override  fun onStart() {
+        super.onStart()
+        loginDialog()
+    }
+
+    fun loginDialog() {
+        val builder = AlertDialog.Builder(this)
+
+        with(builder) {
+            setTitle("Login")
+            val linearLayout: LinearLayout = LinearLayout(this@MainActivity)
+            linearLayout.orientation = LinearLayout.VERTICAL
+
+            val inputEmail: EditText = EditText(this@MainActivity)
+            inputEmail.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            inputEmail.hint = "Enter email"
+            linearLayout.addView(inputEmail)
+
+            val inputPw: EditText = EditText(this@MainActivity)
+            inputPw.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_NUMBER_VARIATION_PASSWORD
+            inputPw.hint= "Enter password"
+            linearLayout.addView(inputPw)
+            builder.setView(linearLayout)
+
+            builder.setPositiveButton("OK") {dialog, which ->
+                login(inputEmail.text.toString(), inputPw.text.toString())
+            }.show()
+        }
+    }
+
+    fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInwithEmail:success")
+                    currentUser =auth.currentUser
+                } else {
+                    Log.w(TAG, "signInwithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
